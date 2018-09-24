7ce02ace24cc21b84fd9d3d901da6034c43db1a6
dacors <dacorsmntll@gmail.com>
2018-09-23 22:18:47 -0500

    stable version working with DFA

--- Automaton.py
+++ Automaton.py
@@ -1,8 +1,18 @@
 import re
 class Automaton:
 
-    def __init__(self, automaton_name):
+    def __init__(self, automaton_name, states=None, edges=None):
         self.automaton_name = automaton_name
+
+        if states is None :
+            self.states = []
+        else:
+            self.states = states
+
+        if edges is None :
+            self.edges = []
+        else:
+            self.edges = edges
 
 
 class State:
@@ -10,6 +20,7 @@ class State:
     def __init__(self, state_id,is_initial, is_final, edges=None):
         self.state_id = state_id
         self.is_initial = is_initial
+        self.is_final = is_final
         if edges is None:
             self.edges = []
         else: 
@@ -40,8 +51,12 @@ class Edge:
 
 class DFA(Automaton):
 
-    def __init__(self, automaton, states=None, edges=None):
-        self.automaton = automaton
+    def __init__(self, automaton,initial_state, final_state, states=None, edges=None):
+        super().__init__(automaton)
+        self.initial_state = initial_state
+        self.final_state = final_state
+        if any(x for x in edges if len(x.edge_accepted_values) == 0) == True:
+            return None
         if states is None:
             self.states = [] 
             self.edges = []
@@ -51,14 +66,35 @@ class DFA(Automaton):
                 self.edges = []
             else:
                 self.edges = edges
+        
 
-    def evaluate_expresion_by_automaton(self, expresion_str):
-        pass
-    
+    def evaluate_expresion_by_automaton(self, expresion_str, actual_state):  
+        
+        if expresion_str != "":
+            expresion_arr = list(expresion_str)
+            for x in range(0,len(actual_state.edges)):
+                for z in range(0, len(expresion_arr)):
+                    if any(expresion_arr[z] in s for s in actual_state.edges[x].edge_accepted_values):
+                        if expresion_str != "":
+                            expresion_arr.remove(expresion_arr[x])
+                            expresion_str = ''.join(expresion_arr)
+                            actual_state = next((a for a in self.states if a.state_id == actual_state.edges[x].final_state), None)
+                            return self.evaluate_expresion_by_automaton(expresion_str, actual_state)
+                        elif actual_state.is_final and expresion_str == "" :
+                            return True
+                    else:
+                        return False 
+        elif actual_state.is_final:
+            return True
+        else:
+            return False
+ 
     @classmethod
     def read_from_str(cls, automaton_str):
         automaton_arr = list(filter(lambda x: len(x)>0,re.split("\n", automaton_str)))  
         edges = []
+        initial_state = ""
+        final_state = ""
         states = []
         states_tmp = []
         for x in range(0,len(automaton_arr)):
@@ -71,16 +107,28 @@ class DFA(Automaton):
             is_initial = ">" in states_arr[x]
             state_id = ''.join(re.findall(r"[\w']+", states_arr[x]))
             state_edges = list(filter(lambda x: x.initial_state == state_id, edges))   
-            states.append(State(state_id, is_initial, is_final, edges))
+            state = State(state_id, is_initial, is_final, state_edges)
+            states.append(state)
+            if  is_initial:
+                initial_state = state
+            if is_final:
+                final_state = state
 
-        return DFA("automatonDAF", states, edges)
+        return DFA("automatonDAF", initial_state, final_state, states, edges)
 
    
 automaton_file = open("automata.txt")
 automaton_string = automaton_file.read()
 automatonDFA = DFA.read_from_str(automaton_string)
-expresion_str = input("write an expresion and press enter to evaluate in automaton:  ")
-automatonDFA.evaluate_expresion_by_automaton(expresion_str)
+if automatonDFA == None:
+    print("Syntax error, check the automaton")
+else:
+    run_program = True
+    while run_program:
+        expresion_str = input("write an expresion and press enter to evaluate in automaton:  ")
+        if expresion_str == "false":
+            run_program = False
+        print(automatonDFA.evaluate_expresion_by_automaton(expresion_str, automatonDFA.initial_state))
 
 
 
