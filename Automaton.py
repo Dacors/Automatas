56b30db27b04aec172acdbfd7d75dcb5cb149905
dacors <dacorsmntll@gmail.com>
2018-10-17 20:34:09 -0500

    the ndfa is new

--- Automaton.py
+++ Automaton.py
@@ -1,5 +1,8 @@
 import re
+
 class Automaton:
+
+    ndfa = False
 
     def __init__(self, automaton_name, states=None, edges=None):
         self.automaton_name = automaton_name
@@ -14,6 +17,36 @@ class Automaton:
         else:
             self.edges = edges
 
+    @classmethod
+    def read_from_str(cls, automaton_arr):
+        automaton_type = ""
+        edges = []
+        initial_state = ""
+        final_state = ""
+        states = []
+        states_tmp = []
+        for x in range(0,len(automaton_arr)):
+            edges.append(Edge.read_from_str(automaton_arr[x]))
+            states_tmp.extend(list(filter(lambda x: '(' in x, re.split(",",automaton_arr[x]))))
+        
+        states_arr = list(set(states_tmp))
+        for x in range(0,len(states_arr)):
+            is_final = "((" in states_arr[x]
+            is_initial = ">" in states_arr[x]
+            state_id = ''.join(re.findall(r"[\w']+", states_arr[x]))
+            state_edges = list(filter(lambda x: x.initial_state == state_id, edges))
+            if len(state_edges[0].edge_accepted_values) == 0:
+                Automaton.ndfa = True
+            state = State(state_id, is_initial, is_final, state_edges)
+            states.append(state)
+            if  is_initial:
+                initial_state = state
+            if is_final:
+                final_state = state
+        if Automaton.ndfa:
+            return NDFA("automatonDFA", initial_state, final_state, states, edges)
+        else:
+            return DFA("automatonDFA", initial_state, final_state, states, edges)
 
 class State:
 
@@ -47,7 +80,6 @@ class Edge:
         accepted_values = re.findall(r'\d+',edge_str)
         edge = Edge(''.join(re.findall(r"[\w']+", states_arr[0])), ''.join(re.findall(r"[\w']+", states_arr[1])), recursive, accepted_values )
         return edge
-
 
 class DFA(Automaton):
 
@@ -88,47 +120,49 @@ class DFA(Automaton):
             return True
         else:
             return False
- 
-    @classmethod
-    def read_from_str(cls, automaton_str):
-        automaton_arr = list(filter(lambda x: len(x)>0,re.split("\n", automaton_str)))  
-        edges = []
-        initial_state = ""
-        final_state = ""
-        states = []
-        states_tmp = []
-        for x in range(0,len(automaton_arr)):
-            edges.append(Edge.read_from_str(automaton_arr[x]))
-            states_tmp.extend(list(filter(lambda x: '(' in x, re.split(",",automaton_arr[x]))))
+
+class NDFA(Automaton):
+
+    def __init__(self, automaton,initial_state, final_state, states=None, edges=None):
+        super().__init__(automaton)
+        self.initial_state = initial_state
+        self.final_state = final_state
+        if any(x for x in edges if len(x.edge_accepted_values) == 0) == True:
+            return None
+        if states is None:
+            self.states = [] 
+            self.edges = []
+        else:
+            self.states = states
+            if edges is None:
+                self.edges = []
+            else:
+                self.edges = edges
         
-        states_arr = list(set(states_tmp))
-        for x in range(0,len(states_arr)):
-            is_final = "((" in states_arr[x]
-            is_initial = ">" in states_arr[x]
-            state_id = ''.join(re.findall(r"[\w']+", states_arr[x]))
-            state_edges = list(filter(lambda x: x.initial_state == state_id, edges))   
-            state = State(state_id, is_initial, is_final, state_edges)
-            states.append(state)
-            if  is_initial:
-                initial_state = state
-            if is_final:
-                final_state = state
 
-        return DFA("automatonDAF", initial_state, final_state, states, edges)
-
-   
+    def evaluate_expresion_by_automaton(self, expresion_str, actual_state):  
+        return True
+ 
 automaton_file = open("automata.txt")
 automaton_string = automaton_file.read()
-automatonDFA = DFA.read_from_str(automaton_string)
-if automatonDFA == None:
-    print("Syntax error, check the automaton")
-else:
-    run_program = True
-    while run_program:
-        expresion_str = input("write an expresion and press enter to evaluate in automaton:  ")
-        if expresion_str == "false":
-            run_program = False
-        print(automatonDFA.evaluate_expresion_by_automaton(expresion_str, automatonDFA.initial_state))
+automaton_arr = list(filter(lambda x: len(x)>0,re.split("\n", automaton_string))) 
+run_program = True
+
+while run_program:
+    
+    if automaton_arr[0] == '1':
+        automaton_arr.pop(0)
+        obj_automaton = Automaton.read_from_str(automaton_arr)
+        if obj_automaton == None:
+            print("Syntax error, check the automaton")
+        else:
+            expresion_str = input("write an expresion and press enter to evaluate in automaton:  ")
+            if expresion_str == "false":
+                run_program = False
+            print(obj_automaton.evaluate_expresion_by_automaton(expresion_str, obj_automaton.initial_state))
+    elif automaton_arr[0] == '2':
+        automaton_arr.pop(0)
+        obj_automaton = Automaton.read_from_str(automaton_arr)
 
 
 
